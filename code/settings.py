36bd9ea94c96e0d61a8ed2a144bccffc3a859989
Leonardo <leonardo.kanashiro.felizardo@gmail.com>
2022-04-05 09:10:53 -0300

    structure update

--- code/settings.py
+++ code/settings.py
@@ -4,5 +4,5 @@ from pathlib import Path
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_DIR = os.path.join(Path(BASE_DIR).parent, 'code', 'data')
-RESULTS_DIR = os.path.join(Path(BASE_DIR).parent, 'code', 'results')
+RESULTS_DIR = os.path.join(Path(BASE_DIR).parent,'data')
 SAVE_PARAMS = os.path.join(Path(BASE_DIR).parent, 'code', 'params')
